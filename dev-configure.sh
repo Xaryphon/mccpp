f1aa64af78cef0472b6b75dd5435d1cebc447be7
Xaryphon <xaryphon@tuta.io>
2022-11-28 10:15:46 +0200

    Build system improvements

--- dev-configure.sh
+++ dev-configure.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+
+CXXFLAGS="-Wall -Wpedantic -Wextra -Werror"
+
+# Building glm on c++20 requires this
+CXXFLAGS="$CXXFLAGS -Wno-volatile"
+
 cmake -S . -B build -GNinja -Werror=dev \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DCMAKE_CXX_FLAGS="-Wall -Wpedantic -Wextra -Werror" \
-    -DCMAKE_CXX_DEBUG_FLAGS="-fsanitize=address,undefined" \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+    -DCMAKE_CXX_FLAGS_DEBUG="-fsanitize=address,undefined" \
     "$@"
