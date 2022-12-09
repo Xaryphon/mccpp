d7c11e6ff423cf9a4f3775fd0e590b8cf1de7ff3
Xaryphon <xaryphon@tuta.io>
2022-12-09 23:24:49 +0200

    Improved read_file

--- src/resource/manager.cc
+++ src/resource/manager.cc
@@ -2,6 +2,8 @@
 
 #include <fstream>
 
+#include "../logger.hh"
+
 namespace mccpp::resource::manager {
 
 utility::runtime_array<char> read_file(const char *path)
@@ -9,12 +11,20 @@ utility::runtime_array<char> read_file(const char *path)
     std::filebuf *filebuf;
     std::ifstream stream;
     stream.open(path, std::ios_base::in | std::ios_base::binary);
+    if (stream.fail()) {
+        MCCPP_E("Failed to open file {}", path);
+        return utility::runtime_array<char>();
+    }
     filebuf = stream.rdbuf();
 
-    size_t size = filebuf->pubseekoff(0, std::ios::end, std::ios::in);
+    auto size = filebuf->pubseekoff(0, std::ios::end, std::ios::in);
+    if (size < 0) {
+        MCCPP_E("Failed to seek to end of {}", path);
+        return utility::runtime_array<char>();
+    }
     filebuf->pubseekpos(0, std::ios::in);
 
-    utility::runtime_array<char> buffer { size };
+    utility::runtime_array<char> buffer { (size_t)size };
     filebuf->sgetn(buffer.data(), buffer.size());
 
     stream.close();
